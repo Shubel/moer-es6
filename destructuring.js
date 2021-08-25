212b848071114fd882ca7163493114056500f1b4
Sunny <shubelislam.diu@gmail.com>
2021-08-25 20:11:25 +0600

    example added

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,21 @@
+// 1st example
+const company = {name: "programming hero", work: "web development", employee: 27, student: 5200};
+
+// const name = company.name;
+// const work = company.work;
+// const employee = company.employee;
+// const student = company.student;
+
+// console.log(name);
+// console.log(work);
+// console.log(employee);
+// console.log(student);
+
+const {name, work, employee, student} = company;
+console.log(name, work, employee, student);
+
+
+// 2nd example
 const student = {
     name: "Kuddus Ali",
     roll: 744,
